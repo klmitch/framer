1b7d644fac17b38bbcece8ac5152e5cc322504fa
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2015-08-25 17:56:46 -0500

    Fix asyncio import
    The trollius library no longer installs as the 'asyncio' package, but
    as 'trollius'.  Change the one import we have to import trollius as
    asyncio if asyncio is not present.

--- framer/protocol.py
+++ framer/protocol.py
@@ -14,7 +14,10 @@
 # along with this program.  If not, see
 # <http://www.gnu.org/licenses/>.
 
-import asyncio
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
 
 
 class FramedProtocol(asyncio.BaseProtocol):
